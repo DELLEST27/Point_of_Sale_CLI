c82cc20ee8fb8088835e80d4910ea1e4653db13c
DELLEST27 <jaidell.kennedy@stu.vertexacademies.org>
2026-04-14 18:16:39 +0000

    kennedy

--- module.py
+++ module.py
@@ -4,18 +4,18 @@
 
 # You will need to add code in this module to make teh Point of Sale Application functional.
 
-
+print("welcome to Kennedy's market")
+print("1. check your cart")
+print("2. add to thy cart")
+print("3. show total price")
+print("4. checkout")
+print("5. Exit")
 class PointOfSale:
   def __init__(self):
     self.checkout_total = 0 # This is an example varialbe, remove it or change it as you please.
     print("\nInitializing POS system...")
     self.cart= []
-    Pizza = [Pizza, 10]
-    Steak = [Steak, 8.50]
-    Burger= [Burger, 10]
-    Fries = [Fries, 7]
-    
-    
+   
     
   def start(self): # This is the function that should be used to start the application.
     print("\nThe app is not complete.")
